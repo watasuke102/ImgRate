dc485b5ebbca2eefb1871860d4778cf222d36e5d
watasuke102 <watasuke102@gmail.com>
2023-05-28 02:20:32 +0900

    update: back>change execute method
    use `go run .`, and change the path to specify sqlite.db

--- back/main.go
+++ back/main.go
@@ -6,8 +6,6 @@ import (
 	"main/db"
 	"main/graph"
 	"net/http"
-	"os"
-	"path/filepath"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -16,17 +14,7 @@ import (
 )
 
 func main() {
-	executable_name, err := os.Executable()
-	if err != nil {
-		log.Fatalln("Failed to get the path to Executable")
-	}
-
-	path := filepath.Join(filepath.Dir(executable_name), "../sqlite")
-	if err := os.Mkdir(path, 0755); err != nil && !os.IsExist(err) {
-		log.Fatalln("Failed to create 'sqlite' dir")
-	}
-
-	database, err := sql.Open("sqlite", filepath.Join(path, "sqlite.db"))
+	database, err := sql.Open("sqlite", "sqlite/sqlite.db")
 	if err != nil {
 		log.Fatalln("Failed to open sqlite.db")
 	}
@@ -40,6 +28,8 @@ func main() {
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", "8080")
+	log.Println("Listening http://localhost:8080")
+	log.Println("  /      : GraphQL playground")
+	log.Println("  /query : GraphQL endpoint")
 	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
