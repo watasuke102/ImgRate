4d4e617e4f96857fe8f39a86dd30861ef91c5ece
watasuke102 <watasuke102@gmail.com>
2023-05-28 10:29:00 +0900

    fix: front>remove vanilla-extract from next.config.js

--- front/next.config.js
+++ front/next.config.js
@@ -1,10 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const {
-  createVanillaExtractPlugin
-} = require('@vanilla-extract/next-plugin');
-const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {};
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = nextConfig;
