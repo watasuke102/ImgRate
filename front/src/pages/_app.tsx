2009d11371bd483e6c7e4fd591881bcef1873155
watasuke102 <watasuke102@gmail.com>
2023-06-03 15:17:56 +0900

    update: front>add title tag and icon

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -9,11 +9,18 @@ import {ChakraProvider} from '@chakra-ui/react';
 import {AppProps} from 'next/app';
 import '@/common/global.css';
 import {theme} from '@/common/theme';
+import Head from 'next/head';
 
 export default function RootLayout(props: AppProps): JSX.Element {
   return (
-    <ChakraProvider theme={theme}>
-      <props.Component {...props.pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>ImGrate</title>
+        <link rel='icon' href='/dummy.png' />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <props.Component {...props.pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
